46b97841512294a278b319148ca19d806b1d9d25
Isabel <isabeldaw608@gmail.com>
2025-10-07 17:28:00 +0200

    ADD files

---
+++ exercise3/exercise3.js
@@ -0,0 +1,11 @@
+function turnOn(){
+    document.getElementById('myImage').src='./images/pic_bulbon.gif'
+    document.getElementById('myMessage').innerHTML= 'La luz esta encencida'
+}
+
+
+function turnOff(){
+    document.getElementById('myImage').src='./images/pic_bulboff.gif'
+    document.getElementById('myMessage').innerHTML= 'La luz esta apagadisima'
+
+}
